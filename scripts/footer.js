76cf0c845224474cfbc977fc2dc49bf00e893f17
mgcvale <miguelcvalealt@gmail.com>
2024-05-11 21:54:34 -0300

    fixed footer, added some anchors and revised content in general

---
+++ scripts/footer.js
@@ -0,0 +1,39 @@
+// this will check if the footer needs to be fixed in the bottom of the page, in the case that it isn't scrollable
+
+
+let previousBodyHeight = getBodyHeight();
+chcekFooter();
+
+setInterval(() => {
+    const currentBodyHeight = getBodyHeight();
+    if(previousBodyHeight != currentBodyHeight) {
+        previousBodyHeight = currentBodyHeight;
+        chcekFooter();
+    }
+}, 1000);
+
+function chcekFooter() {
+    const bodyHeight = getBodyHeight();
+
+    if (bodyHeight == window.innerHeight) {
+        fixFooter();
+    } else {
+        unfixFooter();
+    }
+}
+
+function getBodyHeight() {
+    const body = document.body;
+    const html = document.documentElement;
+    return Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+}
+
+function fixFooter() {
+    document.getElementsByTagName("footer")[0].id = "incomplete-footer";
+    document.getElementById("main").className = "incomplete";
+}
+
+function unfixFooter() {
+    document.getElementsByTagName("footer")[0].id = "";
+    document.getElementById("main").className = "";
+}
