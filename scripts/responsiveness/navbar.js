a9dfb63ccfc0fa0766174a15152679b78e984d1f
mgcvale <miguelcvalealt@gmail.com>
2024-05-27 22:37:01 -0300

    Melhorar responsividade da header

--- scripts/responsiveness/navbar.js
+++ scripts/responsiveness/navbar.js
@@ -1,12 +1,11 @@
 
 function openNav() {
-    const nav = document.getElementsByTagName("nav")[0]
-    console.log("LKSAJDLSAKJD")
-    if(nav.className != "open-nav") {
-        nav.className = "open-nav";
-        document.getElementById("open-nav-button-img").src="/assets/images/icons/website/close.png";
+    const nav = document.getElementsByTagName("nav")[0];
+    if(nav.className != "opened-nav") {
+        nav.className = "opened-nav";
+        document.querySelector("#nav-opener button img").src="/assets/images/icons/website/close.png";
     } else {
         nav.className = "";
-        document.getElementById("open-nav-button-img").src="/assets/images/icons/website/menu.png";
+        document.querySelector("#nav-opener button img").src="/assets/images/icons/website/menu.png";
     }
 }
