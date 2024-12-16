0dade8375f364c15843aabad87822eec8aa7627f
mgcvale <miguelcvalealt@gmail.com>
2024-12-16 09:52:41 -0300

    fix tailwindcss configs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import Footer from "$lib/components/Footer.svelte";
   import Header from "$lib/components/Header.svelte";
-  import "/src/style/tailwind.css";
   import "/src/style/main.scss";
 </script>
 
---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+
+  theme: {
+    extend: {}
+  },
+
+  darkMode: "class",
+
+  plugins: []
+} as Config;
